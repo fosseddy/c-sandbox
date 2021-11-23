ed581ceeaed8f2a72cf3596770172eb3e34459b4
Mark Fosseddy <markfosseddy@gmail.com>
2021-11-23 20:59:21 +0500

    add s_index

--- sandbox/5-1.c
+++ sandbox/5-1.c
@@ -5,28 +5,50 @@
 void s_cat(char *, const char *);
 unsigned int s_end(const char *, const char *);
 void s_reverse(char *);
-int s_index(const char *, const char *)
+int s_index(const char *, const char *);
 
 int main(void)
 {
+    // s_cat
     char s[15] = "hello";
-
     s_cat(s, ", world");
     assert(strcmp(s, "hello, world") == 0);
 
     s[0] = '\0';
+    s_cat(s, "");
+    assert(strcmp(s, "") == 0);
 
+    s[0] = '\0';
     s_cat(s, "this is test");
     assert(strcmp(s, "this is test") == 0);
 
 
+    // s_end
     assert(s_end("hello, world", "rld") == 1);
     assert(s_end("hello, world", ", world") == 1);
     assert(s_end("hello, world", "rd") == 0);
+    assert(s_end("", "") == 1);
 
+
+    // s_reverse
     char x[10] = "hello";
     s_reverse(x);
     assert(strcmp(x, "olleh") == 0);
+
+    s_reverse(s);
+    assert(strcmp(s, "tset si siht") == 0);
+
+    x[0] = '\0';
+    s_reverse(x);
+    assert(strcmp(x, "") == 0);
+
+
+    // s_index
+    assert(s_index("hello, world", "hello") == 0);
+    assert(s_index("hello, world", "rld") == 9);
+    assert(s_index("hello, world", "d") == 11);
+    assert(s_index("hello, world", "rd") == -1);
+    assert(s_index("", "") == -1);
 
     return 0;
 }
@@ -53,6 +75,9 @@ unsigned int s_end(const char *s, const char *t)
 
 void s_reverse(char *s)
 {
+    unsigned int len = strlen(s);
+    if (len == 0) return;
+
     unsigned int offset = strlen(s) - 1;
     char *s_end = s + offset;
 
@@ -65,4 +90,24 @@ void s_reverse(char *s)
 
 int s_index(const char *s, const char *t)
 {
+    unsigned int i = 0;
+
+    while (*s) {
+        const char *sub_s = s;
+        const char *t_copy = t;
+
+        while (*t_copy && *sub_s == *t_copy) {
+            ++sub_s;
+            ++t_copy;
+        }
+
+        if (*t_copy == '\0') {
+            return i;
+        }
+
+        ++i;
+        ++s;
+    }
+
+    return -1;
 }
