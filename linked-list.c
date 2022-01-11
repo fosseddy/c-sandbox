df2a4fb7184ff0e0253a022f17dc29eb9c08a3c9
Mark Fosseddy <markfosseddy@gmail.com>
2022-01-11 19:42:34 +0500

    delete constr

--- linked-list.c
+++ linked-list.c
@@ -2,26 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
-struct Node {
-    int value;
-    struct Node *next;
-};
-
-struct Node *make_node(int val)
-{
-    struct Node *n = malloc(sizeof(struct Node));
-    assert(n != NULL);
-
-    n->value = val;
-    n->next = NULL;
-
-    return n;
-}
-
 int main(void)
 {
-    struct Node *head = make_node(69);
-
     printf("\n");
 
     if (a == NULL) {
