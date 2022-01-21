497bb6cb7ace50d4e1d74f6159ca6096b9e0f3f6
Mark Fosseddy <markfosseddy@gmail.com>
2022-01-21 16:37:41 +0500

    add ex 2

--- os-tep/process-api.c
+++ os-tep/process-api.c
@@ -25,9 +25,32 @@ void change_var(void)
     printf("pid: %i, x: %i\n", child_pid, x);
 }
 
+void opens_file(void)
+{
+    int fd = open("temp", O_RDWR | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
+
+    pid_t child_pid = fork();
+
+    if (child_pid < 0) {
+        fprintf(stderr, "could not create child process\n");
+        exit(1);
+    } else if (child_pid == 0) {
+        printf("child fd: %i\n", fd);
+        char *buf = "This is\n";
+        write(fd, buf, strlen(buf));
+    } else {
+        printf("parent fd: %i\n", fd);
+        char *buf = "test message\n";
+        write(fd, buf, strlen(buf));
+    }
+
+    close(fd);
+}
+
 int main(void)
 {
-    change_var();
+    //change_var();
+    opens_file();
 
     return 0;
 }
