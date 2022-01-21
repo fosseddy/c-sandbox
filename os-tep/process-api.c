1e97ffc7005fa2542d6378bf15c8311ecd721824
Mark Fosseddy <markfosseddy@gmail.com>
2022-01-21 16:36:57 +0500

    add ex 1

--- os-tep/process-api.c
+++ os-tep/process-api.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
 void change_var(void)
 {
