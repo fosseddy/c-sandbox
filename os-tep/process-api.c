29f64f8f0687a19f1c4b40d7a36e094ffa0e39d1
Mark Fosseddy <markfosseddy@gmail.com>
2022-01-21 13:37:31 +0500

    Ready, Set, Go!

---
+++ os-tep/process-api.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void change_var(void)
+{
+    int x = 100;
+
+    pid_t child_pid = fork();
+
+    if (child_pid < 0) {
+        fprintf(stderr, "could not create child process\n");
+        exit(1);
+    } else if (child_pid == 0) {
+        x += 13;
+    } else {
+        x += 8;
+    }
+
+    printf("pid: %i, x: %i\n", child_pid, x);
+}
+
+int main(void)
+{
+    change_var();
+
+    return 0;
+}
+
