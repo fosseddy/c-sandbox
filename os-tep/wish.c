7cfbef01b1c14d0573a223375b8285aad8605e64
Mark Fosseddy <markfosseddy@gmail.com>
2022-01-26 21:01:58 +0500

    basic parallel commands

--- os-tep/wish.c
+++ os-tep/wish.c
@@ -28,7 +28,6 @@ enum Cmd_Kind {
     BUILT_IN_EXIT,
     BUILT_IN_PATH,
     BUILT_IN_CD,
-    BUILT_IN_DUMP,
     CMD_KIND_LENGTH,
 };
 
@@ -37,7 +36,6 @@ static char *built_in_cmds[CMD_KIND_LENGTH] = {
     [BUILT_IN_CD]   = "cd",
     [BUILT_IN_EXIT] = "exit",
     [BUILT_IN_PATH] = "path",
-    [BUILT_IN_DUMP] = "dump"
 };
 
 struct Cmd {
@@ -82,87 +80,102 @@ int main(void)
 
         if (strlen(input) == 0) continue;
 
-        struct Cmd *cmd = malloc(sizeof(struct Cmd));
-        assert(cmd != NULL);
-
-        cmd->redirect = 0;
-        cmd->redirect_dest = NULL;
-
-        cmd->args_size = 0;
-        cmd->args_cap = ARGS_DEFAULT_CAP;
-
-        cmd->kind = NOT_BUILT_IN;
-
-        cmd->args = malloc(cmd->args_cap * sizeof(char *));
-        assert(cmd->args != NULL);
-
+        struct Cmd *buf[10] = {0};
+        size_t sz = 0;
         char *tok = strtok(input, " ");
+
         while (tok != NULL) {
-            if (strcmp(tok, ">") == 0) {
-                cmd->redirect = 1;
-                char *dest = strtok(NULL, " ");
-                if (dest == NULL) {
-                    fprintf(stderr, "Provide redirect destination\n");
+            struct Cmd *cmd = malloc(sizeof(struct Cmd));
+            assert(cmd != NULL);
+
+            cmd->redirect = 0;
+            cmd->redirect_dest = NULL;
+
+            cmd->args_size = 0;
+            cmd->args_cap = ARGS_DEFAULT_CAP;
+
+            cmd->kind = NOT_BUILT_IN;
+
+            cmd->args = malloc(cmd->args_cap * sizeof(char *));
+            assert(cmd->args != NULL);
+
+            while (tok != NULL) {
+                if (strcmp(tok, ">") == 0) {
+                    cmd->redirect = 1;
+                    char *dest = strtok(NULL, " ");
+                    if (dest == NULL) {
+                        fprintf(stderr, "Provide redirect destination\n");
+                    } else {
+                        cmd->redirect_dest = strdup(dest);
+                    }
+                } else if (strcmp(tok, "&") == 0) {
+                    tok = strtok(NULL, " ");
+                    break;
                 } else {
-                    cmd->redirect_dest = strdup(dest);
-                }
-            } else {
-                if (cmd->args_size + 1 == cmd->args_cap) {
-                    REALLOC_ARR(cmd->args, cmd->args_cap, char *);
-                }
-
-                cmd->args[cmd->args_size++] = strdup(tok);
-            }
-
-            tok = strtok(NULL, " ");
-        }
-
-        cmd->args[cmd->args_size++] = NULL;
-
-        char *cmd_name = cmd->args[0];
-
-        for (enum Cmd_Kind i = 1; i < CMD_KIND_LENGTH; ++i) {
-            if (strcmp(cmd_name, built_in_cmds[i]) == 0) {
-                cmd->kind = i;
-                break;
-            }
-        }
-
-        if (cmd->kind == NOT_BUILT_IN) {
-            char path[100] = {0};
-
-            int path_exist = 0;
-            for (size_t i = 0; i < shell->paths_size; ++i) {
-                assert(strlen(shell->paths[i]) + strlen(cmd_name) < 100);
-                sprintf(path, "%s/%s", shell->paths[i], cmd_name);
-                if ((path_exist = access(path, X_OK)) == 0) break;
-            }
-
-            if (path_exist < 0) {
-                fprintf(stderr, "command `%s` not found\n", cmd_name);
-            } else {
-                pid_t cid = fork();
-
-                if (cid < 0) {
-                    fprintf(stderr, "Could not create child process\n");
-                } else if (cid == 0) {
-                    if (cmd->redirect) {
-                        int fd;
-                        if ((fd = open(cmd->redirect_dest, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR)) < 0) {
-                            fprintf(stderr, "%s\n", strerror(errno));
-                        } else {
-                            dup2(fd, STDOUT_FILENO);
-                            dup2(fd, STDERR_FILENO);
-                            close(fd);
-                        }
+                    if (cmd->args_size + 1 == cmd->args_cap) {
+                        REALLOC_ARR(cmd->args, cmd->args_cap, char *);
                     }
 
-                    execv(path, cmd->args);
-                    assert(0 && "Unreachable!\n");
-                } else {
-                    waitpid(cid, NULL, 0);
+                    cmd->args[cmd->args_size++] = strdup(tok);
+                }
+
+                tok = strtok(NULL, " ");
+            }
+
+            cmd->args[cmd->args_size++] = NULL;
+
+            char *cmd_name = cmd->args[0];
+
+            for (enum Cmd_Kind i = 1; i < CMD_KIND_LENGTH; ++i) {
+                if (strcmp(cmd_name, built_in_cmds[i]) == 0) {
+                    cmd->kind = i;
+                    break;
                 }
             }
+
+            buf[sz++] = cmd;
+        }
+
+        struct Cmd *cmd = buf[0];
+        if (cmd->kind == NOT_BUILT_IN) {
+            for (size_t i = 0; i < sz; ++i) {
+                struct Cmd *cmd = buf[i];
+                char *cmd_name = cmd->args[0];
+                char path[100] = {0};
+
+                int path_exist = 0;
+                for (size_t j = 0; j < shell->paths_size; ++j) {
+                    assert(strlen(shell->paths[j]) + strlen(cmd_name) < 100);
+                    sprintf(path, "%s/%s", shell->paths[j], cmd_name);
+                    if ((path_exist = access(path, X_OK)) == 0) break;
+                }
+
+                if (path_exist < 0) {
+                    fprintf(stderr, "command `%s` not found\n", cmd_name);
+                } else {
+                    pid_t cid = fork();
+
+                    if (cid < 0) {
+                        fprintf(stderr, "Could not create child process\n");
+                    } else if (cid == 0) {
+                        if (cmd->redirect) {
+                            int fd;
+                            if ((fd = open(cmd->redirect_dest, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR)) < 0) {
+                                fprintf(stderr, "%s\n", strerror(errno));
+                            } else {
+                                dup2(fd, STDOUT_FILENO);
+                                dup2(fd, STDERR_FILENO);
+                                close(fd);
+                            }
+                        }
+
+                        execv(path, cmd->args);
+                        assert(0 && "Unreachable!\n");
+                    }
+                }
+            }
+
+            while (waitpid(-1, NULL, 0) > 0);
         } else {
             switch (cmd->kind) {
                 case BUILT_IN_EXIT:
@@ -195,40 +208,18 @@ int main(void)
                     }
                 } break;
 
-                case BUILT_IN_DUMP:
-                    printf("cmd:\n");
-                    printf("    redirect: %i\n", cmd->redirect);
-                    printf("    redirect_dest: %s\n", cmd->redirect_dest);
-                    printf("    args_size: %lu\n", cmd->args_size);
-                    printf("    args_cap: %lu\n", cmd->args_cap);
-                    printf("    args:\n");
-                    for (size_t i = 0; i < cmd->args_size; ++i) {
-                        printf("        arg %lu: %s\n", i, cmd->args[i]);
-                    }
-
-                    printf("\n");
-
-                    printf("shell:\n");
-                    printf("    paths_size: %lu\n", shell->paths_size);
-                    printf("    paths_cap: %lu\n", shell->paths_cap);
-                    printf("    paths:\n");
-                    for (size_t i = 0; i < shell->paths_size; ++i) {
-                        printf("        path %lu: %s\n", i, shell->paths[i]);
-                    }
-
-                    printf("\n");
-                    break;
-
                 default: assert(0 && "Unreachable\n");
             }
         }
 
-        for (size_t i = 0; cmd->args[i] != NULL; ++i) {
-            free(cmd->args[i]);
+        for (size_t i = 0; i < sz; ++i) {
+            for (size_t j = 0; buf[i]->args[j] != NULL; ++j) {
+                free(buf[i]->args[j]);
+            }
+            free(buf[i]->args);
+            free(buf[i]->redirect_dest);
+            free(buf[i]);
         }
-        free(cmd->args);
-        free(cmd->redirect_dest);
-        free(cmd);
     }
 
     for (size_t i = 0; i < shell->paths_size; ++i) {
