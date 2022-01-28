b6cc69537b6bdd58cbe90c8816862a1cdda85b5c
Mark Fosseddy <markfosseddy@gmail.com>
2022-01-28 16:48:29 +0500

    restructure, refactro path command execution

--- os-tep/wish.c
+++ os-tep/wish.c
@@ -35,9 +35,7 @@ enum Cmd_Kind {
 struct Cmd {
     int redirect;
     char *redirect_dest;
-
     char **args;
-
     enum Cmd_Kind kind;
 };
 
@@ -48,98 +46,47 @@ static char *built_in_cmds[CMD_KIND_LENGTH] = {
     [BUILT_IN_PATH] = "path",
 };
 
-size_t parse_input(char *, struct Cmd **);
-void execute_cmd(struct Cmd *, char **, size_t);
-void free_cmds(struct Cmd *, size_t);
-void free_paths(char **, size_t);
-char *strdup(char *);
-
-int main(void)
+char *strdup(char *src)
 {
-    int exit = 0;
+    char *p = malloc((strlen(src) + 1) * sizeof(char));
+    assert(p != NULL);
 
-    size_t paths_cap = PATHS_CAP;
-    size_t paths_size = 0;
-    char **paths = malloc(paths_cap * sizeof(char *));
-    assert(paths != NULL);
+    strcpy(p, src);
 
-    paths[paths_size++] = strdup(DEFAULT_EXEC_PATH);
+    return p;
+}
 
-    while (!exit) {
-        char input[INPUT_CAP] = {0};
-        fgets(input, INPUT_CAP, stdin);
-        input[strlen(input) - 1] = '\0';
-
-        if (strlen(input) == 0) continue;
-
-        struct Cmd *cmds = NULL;
-        size_t cmds_size = parse_input(input, &cmds);
-
-        if (cmds_size > 1) {
-            int is_valid = 1;
-            for (size_t i = 0; i < cmds_size; ++i) {
-                if (cmds[i].kind != NOT_BUILT_IN) {
-                    is_valid = 0;
-                    break;
-                }
-            }
-
-            if (!is_valid) {
-                fprintf(stderr, "Parallel execution is not allowed "
-                                "with built in commands\n");
-                free_cmds(cmds, cmds_size);
-                continue;
-            }
+int validate_parallel_cmds(struct Cmd *cmds, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        if (cmds[i].kind != NOT_BUILT_IN) {
+            return 0;
         }
-
-        if (cmds[0].kind == NOT_BUILT_IN) {
-            for (size_t i = 0; i < cmds_size; ++i) {
-                execute_cmd(&cmds[i], paths, paths_size);
-            }
-            while (waitpid(-1, NULL, 0) > 0);
-        } else {
-            struct Cmd cmd = cmds[0];
-            switch (cmd.kind) {
-                case BUILT_IN_EXIT:
-                    exit = 1;
-                    break;
-
-                case BUILT_IN_PATH:
-                    for (size_t i = 0; i < paths_size; ++i) {
-                        free(paths[i]);
-                    }
-                    paths_size = 0;
-                    if (cmd.args[1] == NULL) {
-                        paths[paths_size++] = strdup("");
-                    } else {
-                        for (size_t i = 1; cmd.args[i] != NULL; ++i) {
-                            if (paths_size == paths_cap) {
-                                REALLOC_ARR(paths, paths_cap, char *);
-                            }
-                            paths[paths_size++] = strdup(cmd.args[i]);
-                        }
-                    }
-                    break;
-
-                case BUILT_IN_CD: {
-                    char *dir_name = cmd.args[1];
-                    if (dir_name == NULL) {
-                        fprintf(stderr, "No such file or directory\n");
-                    } else if (chdir(dir_name) < 0) {
-                        fprintf(stderr, "%s\n", strerror(errno));
-                    }
-                } break;
-
-                default: assert(0 && "Unreachable\n");
-            }
-        }
-
-        free_cmds(cmds, cmds_size);
     }
 
-    free_paths(paths, paths_size);
+    return 1;
+}
 
-    return 0;
+void free_cmds(struct Cmd *cmds, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        for (size_t j = 0; cmds[i].args[j] != NULL; ++j) {
+            free(cmds[i].args[j]);
+        }
+        free(cmds[i].args);
+        free(cmds[i].redirect_dest);
+    }
+
+    free(cmds);
+}
+
+void free_paths(char **paths, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        free(paths[i]);
+    }
+
+    free(paths);
 }
 
 size_t parse_input(char *input, struct Cmd **bufptr)
@@ -259,34 +206,93 @@ void execute_cmd(struct Cmd *cmd, char **paths, size_t paths_size)
     free(exec_path);
 }
 
-void free_cmds(struct Cmd *cmds, size_t size)
+char **execute_path_cmd(char **args, char **paths, size_t *paths_size)
 {
-    for (size_t i = 0; i < size; ++i) {
-        for (size_t j = 0; cmds[i].args[j] != NULL; ++j) {
-            free(cmds[i].args[j]);
+    free_paths(paths, *paths_size);
+
+    size_t paths_cap = PATHS_CAP;
+    *paths_size = 0;
+
+    paths = malloc(paths_cap * sizeof(char *));
+    assert(paths != NULL);
+
+    if (args[1] == NULL) {
+        paths[(*paths_size)++] = strdup("");
+    } else {
+        for (size_t i = 1; args[i] != NULL; ++i) {
+            if (*paths_size == paths_cap) {
+                REALLOC_ARR(paths, paths_cap, char *);
+            }
+            paths[(*paths_size)++] = strdup(args[i]);
         }
-        free(cmds[i].args);
-        free(cmds[i].redirect_dest);
     }
 
-    free(cmds);
+    return paths;
 }
 
-void free_paths(char **paths, size_t size)
+int main(void)
 {
-    for (size_t i = 0; i < size; ++i) {
-        free(paths[i]);
+    int exit = 0;
+
+    size_t paths_cap = PATHS_CAP;
+    size_t paths_size = 0;
+    char **paths = malloc(paths_cap * sizeof(char *));
+    assert(paths != NULL);
+
+    paths[paths_size++] = strdup(DEFAULT_EXEC_PATH);
+
+    while (!exit) {
+        char input[INPUT_CAP] = {0};
+        fgets(input, INPUT_CAP, stdin);
+        input[strlen(input) - 1] = '\0';
+
+        if (strlen(input) == 0) continue;
+
+        struct Cmd *cmds = NULL;
+        size_t cmds_size = parse_input(input, &cmds);
+
+        if (cmds_size > 1) {
+            if (!validate_parallel_cmds(cmds, cmds_size)) {
+                fprintf(stderr, "Parallel execution is not allowed "
+                                "with built in commands\n");
+                free_cmds(cmds, cmds_size);
+                continue;
+            }
+        }
+
+        if (cmds[0].kind == NOT_BUILT_IN) {
+            for (size_t i = 0; i < cmds_size; ++i) {
+                execute_cmd(&cmds[i], paths, paths_size);
+            }
+            while (waitpid(-1, NULL, 0) > 0);
+        } else {
+            struct Cmd *cmd = &cmds[0];
+            switch (cmd->kind) {
+                case BUILT_IN_EXIT:
+                    exit = 1;
+                    break;
+
+                case BUILT_IN_PATH:
+                    paths = execute_path_cmd(cmd->args, paths, &paths_size);
+                    break;
+
+                case BUILT_IN_CD: {
+                    char *dir_name = cmd->args[1];
+                    if (dir_name == NULL) {
+                        fprintf(stderr, "No such file or directory\n");
+                    } else if (chdir(dir_name) < 0) {
+                        fprintf(stderr, "%s\n", strerror(errno));
+                    }
+                } break;
+
+                default: assert(0 && "Unreachable\n");
+            }
+        }
+
+        free_cmds(cmds, cmds_size);
     }
 
-    free(paths);
-}
+    free_paths(paths, paths_size);
 
-char *strdup(char *src)
-{
-    char *p = malloc((strlen(src) + 1) * sizeof(char));
-    assert(p != NULL);
-
-    strcpy(p, src);
-
-    return p;
+    return 0;
 }
