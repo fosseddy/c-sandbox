0679e35c7fead6ed29dc2159c22561a8d18a9c21
Mark Fosseddy <markfosseddy@gmail.com>
2022-02-03 17:40:17 +0500

    how memory is shared

---
+++ cs-app/threads-3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pthread.h>
+
+#define N 2
+
+void *thread(void *);
+
+char **ptr;
+
+int main(void)
+{
+    char *msgs[N] = {
+        "Hello from foo",
+        "Hello from bar"
+    };
+
+    ptr = msgs;
+
+    for (size_t i = 0; i < N; ++i) {
+        int *arg = malloc(sizeof(int));
+        *arg = i;
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, thread, arg);
+    }
+
+    pthread_exit(NULL);
+}
+
+void *thread(void *vargp)
+{
+    int myid = *((int *) vargp);
+    free(vargp);
+
+    static int cnt = 0;
+
+    printf("[%d]: %s (cnt=%d)\n", myid, ptr[myid], ++cnt);
+
+    return NULL;
+}
