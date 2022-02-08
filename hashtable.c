ad064ddf7befa7194865b5f1c09d741e1883d08c
Mark Fosseddy <markfosseddy@gmail.com>
2022-02-08 21:45:08 +0500

    proto

--- hashtable.c
+++ hashtable.c
@@ -130,6 +130,7 @@ int main(void) {
     hashtable_put(ht, "you", "what");
     hashtable_put(ht, "are", "you");
     hashtable_put(ht, "doing", "today");
+    hashtable_put(ht, "0", "0");
     hashtable_debug(ht);
     printf("-------------------------------------\n");
 
